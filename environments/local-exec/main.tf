2816a9be45c25376a00b129f509bcdd376b6ced9
Namho Kim <git@namho.kim>
2020-04-21 18:14:31 -0700

    var test 3

--- environments/local-exec/main.tf
+++ environments/local-exec/main.tf
@@ -1,9 +1,3 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo hello world"
-  }
-}
-
 resource "null_resource" "varTest" {
   provisioner "local-exec" {
     command = "echo varTest"
@@ -13,6 +7,12 @@ resource "null_resource" "varTest" {
 resource "null_resource" "varTest2" {
   provisioner "local-exec" {
     command = "echo varTest2"
+  }
+}
+
+resource "null_resource" "hello_world" {
+  provisioner "local-exec" {
+    command = "echo hello world"
   }
 }
 
