6b6728b5aa1d3ba88dd13cd1cab2f3232b3d4942
Namho Kim <git@namho.kim>
2020-04-24 19:35:12 -0700

    recreate guido

--- environments/pike/main.tf
+++ environments/pike/main.tf
@@ -2,7 +2,7 @@
 # NEW NEW COMMENT
 # NEW COMMENT
 # NEW NEW NEW COMMENT
-# Guido re-added
+# Guido reCreated
 resource "null_resource" "pikeCreate" {
   provisioner "local-exec" {
     command = "echo pike"
